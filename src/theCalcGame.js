43c344d6cd291385427757e226c86b06f8665b6b
Anton Lipilin <antonlipilinwork@gmail.com>
2021-01-21 00:07:26 +0400

    создал файл theCalcGame.js  и описал в нём логику игры

---
+++ src/theCalcGame.js
@@ -0,0 +1,45 @@
+import readlineSync from 'readline-sync';
+import getUserName from './cli.js';
+
+const calculator = () => {
+  const name = getUserName();
+  let correctAnswersCounter = 0;
+  console.log('What is the result of the expression?');
+
+  while (true) {
+    if (correctAnswersCounter === 3) {
+      console.log(`Congratulations, ${name}!`);
+      return;
+    }
+
+    const operators = ['+', '-', '*'];
+    const firstOperand = Math.round(Math.random() * 30);
+    const secondOperand = Math.round(Math.random() * 30);
+    const randomIndex = Math.floor(Math.random() * operators.length);
+    const randomOperator = operators[randomIndex];
+    console.log(`Question: ${firstOperand} ${randomOperator} ${secondOperand}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    let correctAnswer;
+
+    if (randomOperator === '*') {
+      correctAnswer = firstOperand * secondOperand;
+    } else if (randomOperator === '-') {
+      correctAnswer = firstOperand - secondOperand;
+    } else {
+      correctAnswer = firstOperand + secondOperand;
+    }
+    correctAnswer = String(correctAnswer); // correct to string
+    const isAnswersAreIdentical = userAnswer === correctAnswer;
+
+    if (!isAnswersAreIdentical) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.
+Let's try again, ${name}!`);
+      break;
+    } else {
+      correctAnswersCounter += 1;
+      console.log('Correct!');
+    }
+  }
+};
+
+calculator();
