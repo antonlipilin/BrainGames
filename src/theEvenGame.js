6cbb5e2d47e8b006751dfc7ce92c9a4eeeae0f93
Anton Lipilin <antonlipilinwork@gmail.com>
2021-01-20 21:15:06 +0400

    написал логику игры

---
+++ src/theEvenGame.js
@@ -0,0 +1,34 @@
+import readlineSync from 'readline-sync';
+import getUserName from './cli.js';
+
+const getRandomNumber = () => Math.round(Math.random() * 100);
+const name = getUserName();
+
+const getUserAnswer = () => {
+  let counterCorrectAnswers = 0;
+
+  while (true) {
+    if (counterCorrectAnswers === 3) {
+      console.log(`Congratulations, ${name}!`);
+      return;
+    }
+
+    const number = getRandomNumber();
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+    console.log(`Question: ${number}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    const correctAnswer = (number % 2 === 0) ? 'yes' : 'no';
+    const isAnswersAreIdentical = userAnswer === correctAnswer;
+
+    if (!isAnswersAreIdentical) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.
+Let's try again, ${name}!`);
+      break;
+    } else {
+      console.log('Correct!');
+      counterCorrectAnswers += 1;
+    }
+  }
+};
+
+export default getUserAnswer;
