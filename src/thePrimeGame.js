e59c9f0db05ddb18b315b98a33d7b5938a756e50
Anton Lipilin <antonlipilinwork@gmail.com>
2021-01-22 21:54:57 +0400

    добавил игру brain-prime и  сделал файл исполняемым

---
+++ src/thePrimeGame.js
@@ -0,0 +1,44 @@
+import readlineSync from 'readline-sync';
+import getUserName from './cli.js';
+
+const getRandomNumber = () => Math.floor(Math.random() * 100);
+
+const isPrime = (number) => {
+  if (number < 2) return false;
+  for (let i = 2; i < number; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const primeGame = () => {
+  const name = getUserName();
+  let correctAnswersCounter = 0;
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+  while (true) {
+    if (correctAnswersCounter === 3) {
+      console.log(`Congratulations, ${name}!`);
+      return;
+    }
+
+    const number = getRandomNumber();
+    console.log(`Question: ${number}`);
+    const correctAnswer = (isPrime(number)) ? 'yes' : 'no';
+    const userAnswer = readlineSync.question('Your answer: ');
+    const isAnswersAreIdentical = userAnswer === correctAnswer;
+
+    if (!isAnswersAreIdentical) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.
+  Let's try again, ${name}!`);
+      break;
+    } else {
+      correctAnswersCounter += 1;
+      console.log('Correct!');
+    }
+  }
+};
+
+export default primeGame;
